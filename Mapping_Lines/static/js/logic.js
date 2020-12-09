5710256edf6693273cce529004601558d0e02093
catderohan <cath7489.com>
2020-12-08 22:40:14 -0600

    add file

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -26,15 +26,15 @@ L.polyline(line, {
 let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-})
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
+//cityData.forEach(function(city) {
+    //console.log(city)
+    //L.circleMarker(city.location, {
+        //radius: city.population/100000
+//})
+    //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  //.addTo(map);
   
-});
+//});
 
 // Add a circle to the map
 //L.circleMarker([34.0522, -118.2437], {
